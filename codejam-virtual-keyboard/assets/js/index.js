ca81033f0b808a95d2118abfc6e66c92b99c11e1
Alexander Fraltsov <kolonessens@yandex.ru>
2019-11-06 23:02:22 +0400

    refactor: added eslint and formatted js-files with its

--- codejam-virtual-keyboard/assets/js/index.js
+++ codejam-virtual-keyboard/assets/js/index.js
@@ -1,158 +1,178 @@
 const body = document.querySelector('body');
-body.insertAdjacentHTML('beforeend', `<textarea id="text" name="text" class="textarea" rows="10"></textarea>`);
-body.insertAdjacentHTML('beforeend', `<div class="keyboard"></div>
-<p class="keyboard--guide">Change the language: <span class="keyboard--samples">Shift</span> + <span class="keyboard--samples">Alt</span> , or CLICK HERE</p>`);
+body.insertAdjacentHTML('beforeend', '<textarea id="text" name="text" class="textarea" rows="10"></textarea>');
+body.insertAdjacentHTML('beforeend', '<div class="keyboard"></div><p class="keyboard--guide">Change the language: <span class="keyboard--samples">Shift</span> + <span class="keyboard--samples">Alt</span> , or CLICK HERE</p>');
 
 const keyboardArea = body.querySelector('.keyboard');
 const area = body.querySelector('.textarea');
 let languageForKeys = localStorage.getItem('languageForKeys') || 0;
 let capsMode = 0;
-let keys = {};
+const keys = {};
 
-//initialization:
+// initialization:
 
 (() => {
-  let rows = {};
-  for (let i = 1; i <= 5; i++) {
-    let row = i;
+  const rows = {};
+  for (let i = 1; i <= 5; i += 1) {
+    const row = i;
     keyboardArea.insertAdjacentHTML('beforeend', `<div class="row row${row}"></div>`);
     rows[`row${row}`] = keyboardArea.querySelector(`.row${row}`);
-  };
-  for (let i = 1; i <= 5; i++) {
-    let row = i;
-    for (let j = 0, l = keyboard[`string${row}`].keyCode.length; j < l; j++) {
-      let k = keyboard[`string${row}`].symbol[j];
-      let key = (typeof k === "string")?k:k[languageForKeys];
+  }
+  for (let i = 1; i <= 5; i += 1) {
+    const row = i;
+    for (let j = 0, l = keyboard[`string${row}`].keyCode.length; j < l; j += 1) { // eslint-disable-line no-undef
+      const k = keyboard[`string${row}`].symbol[j];
+      const key = (typeof k === 'string') ? k : k[languageForKeys];
       rows[`row${row}`].insertAdjacentHTML('beforeend', `<div class="button">${key}</div>`);
-    };
-  };
-  for (let i = 1; i <= 5; i++) {
-    let row = rows[`row${i}`];
-    for (let j = 0, l = keyboard[`string${i}`].keyCode.length; j < l; j++) {
-      let key = row.querySelector(`.button:nth-child(${j+1})`);
-      let keyName = keyboard[`string${i}`].keyCode[j];
+    }
+  }
+  for (let i = 1; i <= 5; i += 1) {
+    const row = rows[`row${i}`];
+    for (let j = 0, l = keyboard[`string${i}`].keyCode.length; j < l; j += 1) {
+      const key = row.querySelector(`.button:nth-child(${j + 1})`);
+      const keyName = keyboard[`string${i}`].keyCode[j];
       key.classList.add(`${keyName}`);
       keys[`${keyName}`] = {
-        button : key, 
-        symbol : keyboard[`string${i}`].symbol[j],
+        button: key,
+        symbol: keyboard[`string${i}`].symbol[j],
       };
-    };
-  };
+    }
+  }
 })();
 
-//functions:
+// functions:
 
 const reDraw = (lang, caps, keys) => {
-  for (key in keys) {
-    let keyArr = keys[key]['symbol'];
+  for (const key in keys) {
+    const keyArr = keys[key].symbol;
     if (Array.isArray(keyArr)) {
-      let elem = keys[key]['button'];
+      const elem = keys[key].button;
       elem.textContent = keyArr[+lang + +caps];
-    };
-  };
+    }
+  }
 };
 
 const switchLang = (e) => {
   if (e) {
-    languageForKeys = (languageForKeys===0) ? 2 : 0;
+    languageForKeys = (languageForKeys === 0) ? 2 : 0;
     localStorage.setItem('languageForKeys', languageForKeys);
-    reDraw(languageForKeys,capsMode,keys);
-  };
+    reDraw(languageForKeys, capsMode, keys);
+  }
 };
 
 const shiftSwitch = (keyCode) => {
   const shiftSwitcher = () => {
-    capsMode = (capsMode===0) ? 1 : 0;
-    reDraw(languageForKeys,capsMode,keys);
-    if (capsMode===0) {
-      keys['ShiftLeft']['button'].classList.remove('uppercase');
-      keys['ShiftRight']['button'].classList.remove('uppercase');
+    capsMode = (capsMode === 0) ? 1 : 0;
+    reDraw(languageForKeys, capsMode, keys);
+    if (capsMode === 0) {
+      keys.ShiftLeft.button.classList.remove('uppercase');
+      keys.ShiftRight.button.classList.remove('uppercase');
     } else {
-      keys['ShiftLeft']['button'].classList.add('uppercase');
-      keys['ShiftRight']['button'].classList.add('uppercase');
-    };
+      keys.ShiftLeft.button.classList.add('uppercase');
+      keys.ShiftRight.button.classList.add('uppercase');
+    }
   };
-  switch(keyCode) {
+  switch (keyCode) {
     case 'ShiftLeft': shiftSwitcher();
       break;
     case 'ShiftRight': shiftSwitcher();
       break;
     default: break;
   }
-}
+};
 
 const keyActioned = (keyCode) => {
-  switch(keyCode) {
-    case 'Backspace': area.value = area.value.slice(0,-1);
+  switch (keyCode) {
+    case 'Backspace': {
+      area.value = area.value.slice(0, -1);
       break;
-    case 'Delete': area.value = area.value.slice(1);
+    }
+    case 'Delete': {
+      area.value = area.value.slice(1);
       break;
-    case 'Space': area.value +=" ";
+    }
+    case 'Space': {
+      area.value += ' ';
       break;
-    case 'Enter': area.value +="\n";
+    }
+    case 'Enter': {
+      area.value += '\n';
       break;
-    case 'Tab': area.value +="\t";
+    }
+    case 'Tab': {
+      area.value += '\t';
       break;
-    case 'CapsLock':
-      if (keys["CapsLock"].button.classList.contains('button--active')) {
-        keys["CapsLock"].button.classList.remove('button--active');
-        keys['ShiftLeft']['button'].classList.remove('uppercase');
-        keys['ShiftRight']['button'].classList.remove('uppercase');
+    }
+    case 'CapsLock': {
+      if (keys.CapsLock.button.classList.contains('button--active')) {
+        keys.CapsLock.button.classList.remove('button--active');
+        keys.ShiftLeft.button.classList.remove('uppercase');
+        keys.ShiftRight.button.classList.remove('uppercase');
         capsMode = 0;
-        reDraw(languageForKeys,capsMode,keys);
+        reDraw(languageForKeys, capsMode, keys);
       } else {
-        keys["CapsLock"].button.classList.add('button--active');
-        keys['ShiftLeft']['button'].classList.add('uppercase');
-        keys['ShiftRight']['button'].classList.add('uppercase');
+        keys.CapsLock.button.classList.add('button--active');
+        keys.ShiftLeft.button.classList.add('uppercase');
+        keys.ShiftRight.button.classList.add('uppercase');
         capsMode = 1;
-        reDraw(languageForKeys,capsMode,keys);
-      };
+        reDraw(languageForKeys, capsMode, keys);
+      }
       break;
-    case 'AltLeft': switchLang(event.shiftKey);
+    }
+    case 'AltLeft': {
+      switchLang(event.shiftKey);
       break;
-    case 'AltRight': switchLang(event.shiftKey);
+    }
+    case 'AltRight': {
+      switchLang(event.shiftKey);
       break;
-    case 'ShiftLeft': switchLang(event.altKey);
+    }
+    case 'ShiftLeft': {
+      switchLang(event.altKey);
       break;
-    case 'ShiftRight': switchLang(event.altKey);
+    }
+    case 'ShiftRight': {
+      switchLang(event.altKey);
       break;
-    default: if (!keys[keyCode]) {break;}
-      let arr = keys[keyCode]['symbol'];
+    }
+    default: {
+      if (!keys[keyCode]) break;
+      const arr = keys[keyCode].symbol;
       if (Array.isArray(arr)) {
-        let res = arr[+languageForKeys + +capsMode];
+        const res = arr[+languageForKeys + +capsMode];
         area.value += res;
-      };
-      if (arr === "⇦") area.value += "←";
-      if (arr === "⇨") area.value += "→";
-      if (arr === "⇧") area.value += "↑";
-      if (arr === "⇩") area.value += "↓";
+      }
+      if (arr === '⇦') area.value += '←';
+      if (arr === '⇨') area.value += '→';
+      if (arr === '⇧') area.value += '↑';
+      if (arr === '⇩') area.value += '↓';
       break;
-  };
-}
+    }
+  }
+};
 
-//listeners:
+// listeners:
 
-let pressed = {};
+const pressed = {};
 
 keyboardArea.addEventListener('click', (event) => {
-  if (event.target.classList[0]==='button') {
-    let keyCode = event.target.classList[1];
+  if (event.target.classList[0] === 'button') {
+    const keyCode = event.target.classList[1];
     keyActioned(keyCode);
-  };
+  }
 });
 
 document.addEventListener('keydown', (event) => {
-  if (event.code+'' !== 'F5'&& event.code+'' !== 'F12') {
+  if (event.code !== 'F5' && event.code !== 'F12') {
     event.preventDefault();
   }
-  let keyCode = event.code;
+  const keyCode = event.code;
   if (!keys[keyCode]) return;
   if (keyCode !== 'Backspace' && keyCode !== 'Delete') {
     if (pressed[keyCode]) return;
   }
   pressed[keyCode] = true;
-  for (o in pressed) {
-    let elem = keys[o]['button'];
+  for (const o in pressed) {
+    const elem = keys[o].button;
     elem.classList.add('button--pressed');
   }
   shiftSwitch(keyCode);
@@ -160,9 +180,9 @@ document.addEventListener('keydown', (event) => {
 });
 
 document.addEventListener('keyup', (event) => {
-  let keyCode = event.code;
-  for (o in pressed) {
-    let elem = keys[o]['button'];
+  const keyCode = event.code;
+  for (let o in pressed) { // eslint-disable-line prefer-const
+    const elem = keys[o].button;
     elem.classList.remove('button--pressed');
   }
   delete pressed[keyCode];
@@ -170,16 +190,16 @@ document.addEventListener('keyup', (event) => {
 });
 
 keyboardArea.addEventListener('mousedown', (event) => {
-  if (event.target.classList[0]==='button') {
-    let keyCode = event.target.classList[1];
+  if (event.target.classList[0] === 'button') {
+    const keyCode = event.target.classList[1];
     pressed[keyCode] = true;
     shiftSwitch(keyCode);
   }
 });
 
 keyboardArea.addEventListener('mouseup', (event) => {
-  if (event.target.classList[0]==='button') {
-    let keyCode = event.target.classList[1];
+  if (event.target.classList[0] === 'button') {
+    const keyCode = event.target.classList[1];
     delete pressed[keyCode];
     shiftSwitch(keyCode);
   }
